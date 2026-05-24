96dbdad22b3670dd71cc2b8486b06df05e7cb015
Rajat1-2 <rajatparashar456@gmail.com>
2026-05-24 09:31:12 +0530

    chabra dsa started

--- adsa/stack.cpp
+++ adsa/stack.cpp
@@ -1,88 +1,105 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool isEmpty(vector<int>&st,int top){
-    return top==-1;
-
-}
-bool isfull(vector<int>&st,int top,int n){
-    return top==n-1;
+bool isEmpty(int top){
+    return top == -1;
 }
 
-void push(vector<int>&st,int &top,int val,int n){
-    if(!isfull(st,top,n)){
-        top++;
-        // st.push_back(val);
-        st[top]=val;
-        // return;
-    }
-    else{
-        cout<<"Stack overflow"<<endl;
-    }
-   
-}
-int topele(vector<int>&st,int top){
-    if(!isEmpty(st,top)){
-        return st[top];
-    }
-    else{
-        cout<<"Stack is empty"<<endl;
-        return -1;
-    }
-}
-void pop(vector<int>&st,int &top){
-    if(!isEmpty(st,top)){
-            
-            st.pop_back();
-            top--;
-    }
-    else{
-        cout<<"Stack underflow"<<endl;
-    }
+bool isFull(int top, int n){
+    return top == n - 1;
 }
 
+void push(vector<int>& st, int &top, int val, int n){
+
+    if(isFull(top, n)){
+        cout << "Stack Overflow" << endl;
+        return;
+    }
+
+    top++;
+    st[top] = val;
+}
+
+void pop(vector<int>& st, int &top){
+
+    if(isEmpty(top)){
+        cout << "Stack Underflow" << endl;
+        return;
+    }
+
+    cout << "Popped element: " << st[top] << endl;
+    top--;
+}
+
+void peek(vector<int>& st, int top){
+
+    if(isEmpty(top)){
+        cout << "Stack is Empty" << endl;
+        return;
+    }
+
+    cout << "Top element: " << st[top] << endl;
+}
 
 int main(){
+
     int n;
-    
-    // size of stack
     cin >> n;
-    vector<int>st(n);
-    int top=-1;
-    int k=5;
+
+    vector<int> st(n);
+
+    int top = -1;
+
+    int k = 5;
+
     while(k--){
-         int choice;
-         cin >> choice;
-         switch(choice){
-        case 1:
-            int val;
-            cin >> val;
-            push(st,top,val,n);
-            cout << st[top] << endl;
-            break;
-        case 2:
-            pop(st,top);
-            break;
-        case 3:
-            if(isEmpty(st,top)){
-                cout<<"Stack is empty"<<endl;
-            }
-            else{
-                cout<<"Stack is not empty"<<endl;
-            }
-            break;
-        case 4:
-            if(isfull(st,top,n)){
-                cout<<"Stack is full"<<endl;
-            }
-            else{
-                cout<<"Stack is not full"<<endl;
-            }
-        default:
-            cout<<"Invalid choice"<<endl;
-    }
-    }
-   
 
+        int choice;
+        cin >> choice;
 
+        switch(choice){
+
+            case 1:{
+
+                int val;
+                cin >> val;
+
+                push(st, top, val, n);
+
+                break;
+            }
+
+            case 2:
+
+                pop(st, top);
+                break;
+
+            case 3:
+
+                if(isEmpty(top))
+                    cout << "Stack is Empty" << endl;
+                else
+                    cout << "Stack is Not Empty" << endl;
+
+                break;
+
+            case 4:
+
+                if(isFull(top, n))
+                    cout << "Stack is Full" << endl;
+                else
+                    cout << "Stack is Not Full" << endl;
+
+                break;
+
+            case 5:
+
+                peek(st, top);
+                break;
+
+            default:
+
+                cout << "Invalid Choice" << endl;
+        }
+    }
 }
