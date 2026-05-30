30731f470ab5cffa20144c470e6d4a89a32eea45
Rajat1-2 <rajatparashar456@gmail.com>
2026-05-30 19:06:43 +0530

    amazon  q2

--- tpo.cpp
+++ tpo.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <string>
 #include <algorithm>
 
@@ -8,25 +9,54 @@ int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     
-    string s;
-    if (cin >> s) {
-        int u = 0, p[26] = {0};
-        for (char x : s) {
-            if (!p[x - 'a']) {
-                p[x - 'a'] = 1;
-                u++;
+    int n, m;
+    while (cin >> n >> m) {
+        if (n == 0 || m == 0) {
+            cout << 0 << "\n";
+            continue;
+        }
+        
+        vector<string> g(n);
+        for (int i = 0; i < n; ++i) cin >> g[i];
+        
+        vector<vector<int>> col(n + 1, vector<int>(m, 0));
+        for (int j = 0; j < m; ++j) {
+            for (int i = 0; i < n; ++i) {
+                col[i + 1][j] = col[i][j] + (g[i][j] - '0');
             }
         }
         
-        int n = s.size(), l = 0, ml = n, c = 0, f[26] = {0};
-        for (int r = 0; r < n; r++) {
-            if (f[s[r] - 'a']++ == 0) c++;
-            while (c == u) {
-                ml = min(ml, r - l + 1);
-                if (--f[s[l++] - 'a'] == 0) c--;
+        vector<vector<int>> p(n + 1, vector<int>(m + 1, 0));
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                p[i + 1][j + 1] = p[i][j + 1] + p[i + 1][j] - p[i][j] + (g[i][j] - '0');
             }
         }
-        cout << ml << "\n";
+        
+        auto sum = [&](int r1, int r2, int c1, int c2) {
+            if (r1 > r2 || c1 > c2) return 0;
+            return p[r2 + 1][c2 + 1] - p[r1][c2 + 1] - p[r2 + 1][c1] + p[r1][c1];
+        };
+        
+        int ans = 0;
+        for (int r1 = 0; r1 < n; ++r1) {
+            for (int r2 = r1 + 2; r2 < n; ++r2) {
+                int mw = 1e9;
+                for (int c = 0; c < m; ++c) {
+                    if (g[r1][c] == '1' && g[r2][c] == '1') {
+                        if (col[r2 + 1][c] - col[r1][c] == r2 - r1 + 1) {
+                            if (mw != 1e9) {
+                                ans = max(ans, sum(r1 + 1, r2 - 1, 0, c - 1) - mw);
+                            }
+                            mw = min(mw, sum(r1 + 1, r2 - 1, 0, c));
+                        }
+                    } else {
+                        mw = 1e9;
+                    }
+                }
+            }
+        }
+        cout << ans << "\n";
     }
     return 0;
 }
